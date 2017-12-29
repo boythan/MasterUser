0a335189020bb5cbe405b81a66f5093cc77b4d21
Đỗ Xuân Vinh <vinhdx.k57@gmail.com>
2017-12-29 17:17:41 +0700

    add

--- MasterUser/app/src/main/java/project/doxuanvinh/usermaster/Config.java
+++
@@ -1,9 +0,0 @@
-package project.doxuanvinh.usermaster;
-
-/**
- * Created by Do Xuan Vinh on 25/04/2017.
- */
-
-public class Config {
-    public static final String ROOT_URL = "http://mrpper.fruitful.io/api/";
-}
